a6c8ef2bb88abbb1a344d9956e9a3ba5e551a269
PTR09 <71910300+PTR09@users.noreply.github.com>
2023-10-31 19:09:40 -0500

    Skeleton for Rating Class

---
+++ Rating.java
@@ -0,0 +1,22 @@
+package MentorConnect;
+
+public class Rating
+{
+    private double rating;
+    private int userID;
+    public Rating(){
+
+    }
+    public void createRating(String mentorID, double Rating){
+
+    }
+    public void deleteRating(String mentorID, double Rating){
+
+    }
+    public void editRating(String mentorID, double Rating){
+
+    }
+    public void displayRating(){
+        
+    }
+}
