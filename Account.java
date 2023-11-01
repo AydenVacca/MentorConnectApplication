b6bb09e76070ba6f6f5e89a47397ba0347f1b5f8
SantiagoKipp <147665662+SantiagoKipp@users.noreply.github.com>
2023-10-31 19:06:13 -0500

    Add files via upload
    Signed-off-by: SantiagoKipp <147665662+SantiagoKipp@users.noreply.github.com>

---
+++ Account.java
@@ -0,0 +1,24 @@
+package MentorConnect
+
+public abstract class Account extends User(){
+    //Variables
+    private String username;
+    private String password;
+
+    //Default Constructor(not sure what to add here)
+    public Account(){
+
+    }
+    //Method to Create a new Account
+    public void createAcc(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+    
+    //Method to delete a user's account(possibly think about adding userID as a parameter)
+    public void deleteAcc(){
+        
+    }
+
+
+}
