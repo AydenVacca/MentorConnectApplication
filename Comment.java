f03ce2fbb598d78bdabb181e5a5a824d1edb6e95
PTR09 <71910300+PTR09@users.noreply.github.com>
2023-10-31 19:00:57 -0500

    added methods and variables

--- Comment.java
+++ Comment.java
@@ -1,5 +1,21 @@
 package MentorConnect;
 
 public class Comment {
-    
+    private int commentID;
+    private User user;
+    private String content;
+    private int upvote;
+    private int downvote;
+    public Comment()
+    {
+
+    }
+    public upvote()
+    {
+
+    }
+    public downvote()
+    {
+
+    }
 }
