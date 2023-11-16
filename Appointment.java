6d03b90a12b107b8ff52a92763fd1dd18d201834
SantiagoKipp <147665662+SantiagoKipp@users.noreply.github.com>
2023-11-16 14:24:47 -0600

    Add files via upload
    Signed-off-by: SantiagoKipp <147665662+SantiagoKipp@users.noreply.github.com>

---
+++ Appointment.java
@@ -0,0 +1,10 @@
+package MentorConnect;
+
+public class Appointment {
+    //All the variables required for an appointment object
+    private int appointmentID;
+    private Mentee mentee;
+    private Mentor mentor;
+    private boolean confirmed;
+    
+}
