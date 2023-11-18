440d3fca87ef33280918533491478d707c0414f1
SantiagoKipp <147665662+SantiagoKipp@users.noreply.github.com>
2023-11-17 21:42:46 -0600

    Add files via upload
    Signed-off-by: SantiagoKipp <147665662+SantiagoKipp@users.noreply.github.com>

---
+++ ForumPostService.java
@@ -0,0 +1,30 @@
+package MentorConnect;
+
+public class ForumPostService {
+
+    public void createComment(User user, String content){
+
+
+    }
+    public void deleteComment(Comment comment){
+
+
+    }
+    public void editComment(Comment comment, String newContent){
+
+
+    }
+    public void deleteForum(ForumPost post, User user){
+
+
+    }
+    public void upvote(){
+
+
+    }
+    public void downvote(){
+
+
+    }
+
+}
