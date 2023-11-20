2e028cd499d514775198eaba3c411a0c373e14db
SantiagoKipp <147665662+SantiagoKipp@users.noreply.github.com>
2023-11-19 18:55:31 -0600

    Add files via upload
    Signed-off-by: SantiagoKipp <147665662+SantiagoKipp@users.noreply.github.com>

---
+++ AppointmentService.java
@@ -0,0 +1,32 @@
+package MentorConnect;
+
+public class AppointmentService {
+
+    private Factory factory;
+
+    //Constructor to initialize service with factory
+    public AppointmentService(Factory factory){
+        this.factory=factory;
+    }
+    //Method to cancel appointment
+    public void cancelAppointment(){
+
+    }
+    //Method to create Appointment for Mentee
+    public void createAppointment(){
+
+    }
+    //Method to edit appointment
+    public Appointment editAppointment(){
+
+    }
+    //Method to display appointment
+    public void displayAppointment(){
+
+    }
+    //Method to confirm appointment for mentor
+    public boolean confirmAppointment(){
+        
+    }
+
+}
