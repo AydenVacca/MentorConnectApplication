837f30804623e4c6b696636f23b91ead249d3818
AydenVacca <vaccaayden@gmail.com>
2023-11-30 14:33:57 -0600

    adding rating to main

---
+++ Rating.java
@@ -0,0 +1,25 @@
+public class Rating
+{
+    private double rating;
+    private int userID;
+    // public Rating(){
+    //     this.rating = 0;
+    //     this.userID = ;
+    // }
+    public Rating(int mentorID, double rating){
+        this.userID = mentorID;
+        this.rating = rating;
+    }
+    public void updateRating(int mentorID, double rating){
+
+    }
+    public void deleteRating(int mentorID, double rating){
+
+    }
+    public void editRating(int mentorID, double rating){
+
+    }
+    public void displayRating(){
+
+    }
+}
