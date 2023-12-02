868357cce23105cc5e40975d951859a050aad54b
PTR09 <71910300+PTR09@users.noreply.github.com>
2023-12-02 14:16:36 -0600

    new project

---
+++ src/test/java/com/webapp/mentorconnect2/Mentorconnect2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.mentorconnect2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mentorconnect2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
