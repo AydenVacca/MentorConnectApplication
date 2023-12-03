74bf26fb1511585a917d878fa40fd2d911c8ec69
PTR09 <71910300+PTR09@users.noreply.github.com>
2023-12-03 00:26:00 -0600

    Imported all the java class files

---
+++ src/main/java/SearchService.java
@@ -0,0 +1,13 @@
+public class SearchService {
+    private Factory factory;
+
+    // Constructor to initialize the Factory
+    public SearchService(Factory factory) {
+        this.factory = factory;
+    }
+
+    // Public method to search for a mentor by name
+    public Mentor searchMentor(String mentorName) {
+        return Factory.getMentorByName(mentorName);
+    }
+}
