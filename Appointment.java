6dbcb18decff387a533e9d9bfba1ce9ff6f06b7d
PTR09 <71910300+PTR09@users.noreply.github.com>
2023-12-03 00:43:11 -0600

    Removed java files from outside webapp

--- Appointment.java
+++
@@ -1,9 +0,0 @@
-public class Appointment {
-    //All the variables required for an appointment object
-    private int appointmentID;
-    private Mentee mentee;
-    private Mentor mentor;
-    private boolean confirmed;
-
-    
-}
