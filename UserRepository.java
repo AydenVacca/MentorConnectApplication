bfa00a9c5ea4ec632292e61b05b17fca601322a6
YuboKai <100256089+YuboKai@users.noreply.github.com>
2023-12-08 21:45:12 -0600

    Delete UserRepository.java
    Signed-off-by: YuboKai <100256089+YuboKai@users.noreply.github.com>

--- UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group4.MentorConnect.repository;
-
-import com.group4.MentorConnect.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-// other necessary imports...
-
-public interface UserRepository extends JpaRepository<User, Long>{
-    User findByEmail(String email);
-}
