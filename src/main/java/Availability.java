9f620f6e80625783ded99d7ce17395fae5e75759
YuboKai <100256089+YuboKai@users.noreply.github.com>
2023-12-08 21:46:27 -0600

    Delete src directory
    Signed-off-by: YuboKai <100256089+YuboKai@users.noreply.github.com>

--- src/main/java/Availability.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-
-public class Availability {
-    private List<String> daysAvailable;
-    private Date startTime;
-    private Date endTime;
-}
--- src/main/java/ForumPost.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class ForumPost {
-    private int postID;
-    private String postTitle;
-    private String content;
-    private List<Comment> comments;
-
-}
