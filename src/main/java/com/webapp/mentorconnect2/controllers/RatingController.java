9ed7e668d016d74f582797804348c9596d62d4d0
Yzheng22 <yuhongzheng22@gmail.com>
2023-12-10 14:28:36 -0600

    Save Rating for Mentor and display them

--- src/main/java/com/webapp/mentorconnect2/controllers/RatingController.java
+++ src/main/java/com/webapp/mentorconnect2/controllers/RatingController.java
@@ -1,5 +1,8 @@
 package com.webapp.mentorconnect2.controllers;
 
+import java.util.List;
+
+import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +15,7 @@ import com.webapp.mentorconnect2.models.Rating;
 import com.webapp.mentorconnect2.repository.RatingService;
 
 import jakarta.servlet.http.HttpSession;
+
 
 @Controller
 public class RatingController {
@@ -44,15 +48,29 @@ public String submitRating(HttpSession session,
         redirectAttributes.addFlashAttribute("error", "Rating must be between 1 and 5");
         return "redirect:/rateMentor";
     }
-
     Rating newRating = new Rating();
+    newRating.setMenteeName(menteeName); // Assuming you have a setter method for menteeName
+    newRating.setMentorName(mentorName); // Assuming you have a setter method for mentorName
+    newRating.setRating(rating);         // Assuming you have a setter method for rating
+
+    
     ratingService.save(newRating);
 
     redirectAttributes.addFlashAttribute("successMessage", "Rating submitted successfully!");
     return "redirect:/home";
 }
 
+@GetMapping("/mentorRatings")
+public String mentorRatings(Model model) {
+    // Fetch the average ratings for each mentor
+    var ratings = ratingService.findAverageRatingsByMentor();
 
+    // Add the ratings to the model, making them available to the view
+    model.addAttribute("ratings", ratings);
+
+    // Return the name of the Thymeleaf template that will display these ratings
+    return "mentorRatings";
+}
     // Additional methods can be implemented to view ratings, etc.
 }
 
